7cc5770f3e9a410474dead06a55a8ed5e4d0a20a
Weeds4 <weeds6568@gmail.com>
2024-05-27 23:56:47 +0900

    스토리 빼고 완성

--- TRPG_Game/game.cpp
+++ TRPG_Game/game.cpp
@@ -24,10 +24,10 @@ void Player::displayStatus() const {
     // Set text color back to default (e.g., white text on black background)
     setColor(white, black);
 
-    drawHearts(x - 5, y, health);
-    drawHunger(x + 5, y, hunger);
-    drawMoney(x + 15, y, money);
-    drawWeapon(x + 25, y, weapon);
+    drawHearts(x - 6, y, health,maxHealth);
+    drawHunger(x + 5, y, hunger,maxHunger);
+    drawMoney(x + 16, y, money ,maxMoney);
+    drawWeapon(x + 27, y, items);
 }
 
 int Scene::display() const {
@@ -116,27 +116,69 @@ char Scene::getChoice() const {
     return choice;
 }
 
-bool Scene::isValidChoice(char choice) const {
-    return choices.find(choice) != choices.end();
+bool Scene::isValidChoice(char choice, const Player& player) const {
+    int moneyChange = getMoneyChange(choice);
+    int healthChange = getHealthChange(choice);
+
+    // 플레이어의 배고픔과 돈이 없을 때 체력이 깎이도록 처리
+    if ((player.money + moneyChange < 0 && healthChange < 0) || player.hunger <= 0) {
+        if (player.health - healthChange <= 0) {
+            return false; // 선택이 유효하지 않음
+        }
+    }
+
+    return true; // 선택이 유효함
 }
 
+
 void Game::applySceneEffects(const Scene& scene, char choice) {
+
+    player.maxHealth = 3;
+    player.maxHunger = 3;
+    player.maxMoney = 3;
+   
     player.health += scene.getHealthChange(choice);
     player.hunger += scene.getHungerChange(choice);
     player.money += scene.getMoneyChange(choice);
 
-    if (!scene.getWeaponChange(choice).empty()) {
-        player.weapon = scene.getWeaponChange(choice);
+    std::string weaponChange = scene.getWeaponChange(choice);
+    if (!weaponChange.empty()) {
+        player.weapon = weaponChange;
+        player.items[weaponChange]++;
+
+        if (weaponChange == "방탄복") {
+            player.health += 2;
+        }
     }
 
     if (player.health < 0) player.health = 0;
     if (player.hunger < 0) player.hunger = 0;
-    if (player.money < 0) player.money = 0;
+    if (player.money < 0) player.money = 0;  
+    
+    if (player.health > player.maxHealth) player.health = player.maxHealth;
+    if (player.hunger > player.maxHunger) player.hunger = player.maxHunger;
+    if (player.money > player.maxMoney) player.money = player.maxMoney;
 
-    if (player.health <= 0) {
+
+    if (player.hunger < 0) {
+        player.health += scene.getHungerChange(choice);
+    }
+    if (player.money < 0) {
+        player.health += scene.getMoneyChange(choice);
+    }
+
+    if (player.health < 0) {
         gameOver = true;
     }
+
+    //if (!scene.getWeaponChange(choice).empty()) {
+//    player.weapon = scene.getWeaponChange(choice);
+// 
+// 
+//}
 }
+
+
 
 void Game::addScene(const std::string& sceneId, const Scene& scene) {
     scenes[sceneId] = scene;
@@ -171,35 +213,46 @@ void Game::start(const std::string& startSceneId) {
 
         if (gameOver) {
             EndingDraw();
+            Sleep(3000);
+            system("cls");
+            titleDraw();
+            menuDraw();
             break;
         }
 
-        int x = 43;
-        int y = 13;
-        {
-            system("cls");
-            // 다음 씬으로 넘어갈 때 출력할 메시지를 출력합니다.
-            setColor(white, white);
-            for (int i = x - 12; i <= x + 45; ++i) {
-                gotoxy(i, y - 13);
-                std::cout << " ";
-                gotoxy(i, y + 6);
-                std::cout << " ";
-            }
-            for (int j = y - 13; j <= y + 6; ++j) {
-                gotoxy(x - 12, j);
-                std::cout << " ";
-                gotoxy(x + 45, j);
-                std::cout << " ";
-            }
+        // Scene의 효과 메시지를 출력
+        std::string effectMessage = currentScene.getEffectMessage(choice);
+        if (!effectMessage.empty()) {
+            int x = 43;
+            int y = 13;
+            {
+                system("cls");
+                // 효과 메시지 출력
+                setColor(white, white);
+                for (int i = x - 12; i <= x + 45; ++i) {
+                    gotoxy(i, y - 13);
+                    std::cout << " ";
+                    gotoxy(i, y + 6);
+                    std::cout << " ";
+                }
+                for (int j = y - 13; j <= y + 6; ++j) {
+                    gotoxy(x - 12, j);
+                    std::cout << " ";
+                    gotoxy(x + 45, j);
+                    std::cout << " ";
+                }
 
-            gotoxy(x+1, y -4);  // 선택된 선택지를 표시할 위치
-            setColor(white, black);
-            std::cout << "뭔가 이상한 기운이 느껴진다...";
+                gotoxy(x -4, y - 4);  // 선택된 선택지를 표시할 위치
+                setColor(white, black);
+                std::cout << effectMessage;
+            }
+            Sleep(2000);  // 2초 대기
         }
-        Sleep(2000);  // 2초 대기
 
         currentSceneId = getNextSceneId(choice, currentSceneId);
+        if (currentSceneId == "q") {
+            break;
+        }
     }
 }
 
@@ -208,38 +261,101 @@ void runGame() {
     system("cls");
     Game game;
 
-    Scene scene1(" 저 멀리 사람들이 많이 보인다 ", {
-        {'1', {"다가간다", "2", 0, 0, 0, "맨손"}},
-        {'q', {"지나친다", "q", 0, 0, 0, "맨손"}}
-        },"뭔가 이상한 기운이 느껴진다...");
+    Scene scene1("저 멀리 킬러들이 많이 보인다.", {
+    {'1', {"다가간다", "2", -3, 0, 0, ""}},
+    {'2', {"지나친다", "2", 2, 0, 0, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "당신은 많은 킬러들에게 무참히 살해당했다.."},
+            {'2', "조용히 그 자리를 벗어났다."}
+        });
 
-    Scene scene2("거지를 만났다 그는 나를 잘 알고 있는듯 보인다", {
-        {'1', {"공격한다", "3", -2, 0, 0, ""}},
-        {'2', {"이야기한다", "3", 0, 0, 0, ""}},
-        {'3', {"돈을 준다", "3", 0, 0, -1, ""}}
-        },"뭔가 이상한 기운이 느껴진다...");
+    Scene scene2("길을 가다 예전 동료를 마주했다", {
+      {'1', {"다가간다($ -1)", "3", -2, 0, -1, ""}},
+      {'2', {"지나친다", "4", -1, 0, 0, ""}},
+      {'3', {"공격한다", "4", 0, 0, 1, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "이런.. 당신을 노리고 접근한 동료였습니다.."},
+            {'2', "지나치는 당신을 향해 칼을 던졌습니다."},
+            {'3', "무사히 처리 했습니다."}
+        });
 
-    Scene scene3("예전에 함께 일했던 동료가 보인다", {
-        {'1', {"다가간다", "4", 1, 0, 1, "검"}},
-        {'2', {"무시한다", "1", 0, 0, 0, ""}}
-        },"뭔가 이상한 기운이 느껴진다...");
+    Scene scene3("도망치다 약국을 발견했습니다.", {
+    {'1', {"들어간다($ - 1)", "4", 2, 0, -1, ""}},
+    {'2', {"지나친다", "4", 0, 0, 0, ""}},
+    {'3', {"약사를 위협한다", "4", 2, 0, 1, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "붕대를 구했습니다."},
+            {'2', "무시하고 지나갑니다..."},
+            {'3', "붕대와 현금다발을 훔쳤습니다."}
+        });
     
-    Scene scene4("도망간다", {
-        {'1', {"다가간다", "1", 1, 0, 1, ""}},
-        {'2', {"무시한다", "1", 0, 0, 0, ""}}
-        },"뭔가 이상한 기운이 느껴진다...");
+    Scene scene4("저 멀리서 당신을 저격하고 있습니다.", {
+    {'1', {"숨는다", "5", 0, 0, 0, ""}},
+    {'2', {"빠르게 뛴다", "6", -1, 0, 0, ""}},
+    {'3', {"대응 사격한다", "6", -2, 0, 0, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "근처에 있는 건물로 들어갔습니다."},
+            {'2', "총탄이 당신의 팔을 스쳤습니다.."},
+            {'3', "붕대와 현금다발을 훔쳤습니다."}
+        });
+
+    Scene scene5("마트에 들어왔습니다,다양한 물건이 있습니다.", {
+    {'1', {"붕대($ - 1)", "6", 2, 0, -1, ""}},
+    {'2', {"식빵($ - 1)", "6", 0, 2, -1, ""}},
+    {'3', {"방탄복($ - 1)", "7", 0, 0, -1, "방탄복"}},
+    {'4', {"돌아간다", "6", 0, 0, 0, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "붕대를 감았습니다."},
+            {'2', "허기가 사라졌습니다.."},
+            {'3', "좀 더 든든해졌습니다."},
+            {'4', "무시하고 지나갑니다..."}
+        });
+
+    Scene scene6("지친 몸을 이끌고 당신은 은신처를 찾고 있습니다.", {
+    {'1', {"지하철역", "8", 0, 0, -1, ""}},
+    {'2', {"빈집", "8", 1, 2, 0, ""}},
+    {'3', {"믿을 수 있는 동료의 집", "8", 0, 0, 0, ""}},
+    {'4', {"당신의 집", "8", -1, -1, 0, ""}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "노숙자들이 당신을 지켜봅니다.."},
+            {'2', "다행히 아무도 들어오지 않았습니다.."},
+            {'3', "당신의 동료가 밤동안 당신을 지켜줬습니다.."},
+            {'4', "이런.. 잠복하고 있던 킬러들에게 걸렸습니다.."}
+        });
+
+    Scene scene7("저 멀리 fgfg이 많이 보인다.", {
+    {'1', {"다가간다", "2", -3, 0, 0, "맨손"}},
+    {'2', {"지나친다", "2", 0, 0, 0, "맨손"}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "킬러에게 다가갑니다..."},
+            {'2', "킬러를 무시하고 지나갑니다..."}
+        });
+
+    Scene scene8("저 멀리 fgfg이 많이 보인다.", {
+    {'1', {"다가간다", "2", -3, 0, 0, "맨손"}},
+    {'2', {"지나친다", "2", 0, 0, 0, "맨손"}}
+        }, "뭔가 이상한 기운이 느껴진다...", {
+            {'1', "킬러에게 다가갑니다..."},
+            {'2', "킬러를 무시하고 지나갑니다..."}
+        });
+
+
 
     game.addScene("1", scene1);
     game.addScene("2", scene2);
     game.addScene("3", scene3);
     game.addScene("4", scene4);
+    game.addScene("5", scene5);
+    game.addScene("6", scene6);
+    game.addScene("7", scene7);
+    game.addScene("8", scene8);
 
 
 
     game.start("1");
 }
 
-void drawHearts(int x, int y, int health) {
+void drawHearts(int x, int y, int health, int maxHealth) {
     const std::string heart = "♥";
     const std::string emptyHeart = " ";
 
@@ -247,12 +363,12 @@ void drawHearts(int x, int y, int health) {
     for (int i = 0; i < health; ++i) {
         std::cout << heart << " ";
     }
-    for (int i = health; i < 3; ++i) {
+    for (int i = health; i < maxHealth; ++i) {
         std::cout << emptyHeart << " ";
     }
 }
 
-void drawHunger(int x, int y, int hunger) {
+void drawHunger(int x, int y, int hunger, int maxHunger) {
     const std::string hungerIcon = "Ψ";
     const std::string emptyHunger = " ";
 
@@ -260,12 +376,12 @@ void drawHunger(int x, int y, int hunger) {
     for (int i = 0; i < hunger; ++i) {
         std::cout << hungerIcon << " ";
     }
-    for (int i = hunger; i < 3; ++i) {
+    for (int i = hunger; i < maxHunger; ++i) {
         std::cout << emptyHunger << " ";
     }
 }
 
-void drawMoney(int x, int y, int money) {
+void drawMoney(int x, int y, int money, int maxMoney) {
     const std::string moneyIcon = "$";
     const std::string emptyMoney = " ";
 
@@ -273,12 +389,18 @@ void drawMoney(int x, int y, int money) {
     for (int i = 0; i < money; ++i) {
         std::cout << moneyIcon << " ";
     }
-    for (int i = money; i < 3; ++i) {
+    for (int i = money; i < maxMoney; ++i) { 
         std::cout << emptyMoney << " ";
     }
 }
 
-void drawWeapon(int x, int y, const std::string& weaponName) {
+void drawWeapon(int x, int y, const std::map<std::string, int>& items) {
+    const std::string itemsIcon = "★";
+    const std::string emptyitems = " ";
+
     gotoxy(x, y);
-    std::cout << weaponName;
+    for (const auto& item : items) {
+        std::cout << itemsIcon << " ";
+    }
+    std::cout << std::endl;
 }
--- TRPG_Game/game.h
+++ TRPG_Game/game.h
@@ -10,51 +10,77 @@
 // Player 클래스 선언
 class Player {
 public:
-    Player(int health, int hunger, int money, const std::string& weapon)
-        : health(health), hunger(hunger), money(money), weapon(weapon) {}
+    Player(int health, int maxHealth, int hunger, int maxHunger, int money, int maxMoney, const std::string& weapon)
+        : health(health), maxHealth(maxHealth), hunger(hunger), maxHunger(maxHunger), money(money), maxMoney(maxMoney), weapon(weapon) {}
 
     void displayStatus() const;
 
     void setHealth(int newHealth) {
         if (newHealth < 0) newHealth = 0;
-        if (newHealth > 3) newHealth = 3;
+        if (newHealth > maxHealth) newHealth = maxHealth;
         health = newHealth;
     }
 
+    void setHunger(int newHunger) {
+        if (newHunger < 0) newHunger = 0;
+        if (newHunger > maxHunger) newHunger = maxHunger;
+        hunger = newHunger;
+    }
+
+    void setMoney(int newMoney) {
+        if (newMoney < 0) newMoney = 0;
+        if (newMoney > maxMoney) newMoney = maxMoney;
+        money = newMoney;
+    }
+
     int health;
+    int maxHealth;
     int hunger;
+    int maxHunger;
     int money;
+    int maxMoney;
     std::string weapon;
+    std::map<std::string, int> items;
 };
 
 class Scene {
 public:
-
     Scene() {}
 
-    Scene(const std::string& description, const std::map<char, std::tuple<std::string, std::string, int, int, int, std::string>>& choices, const std::string& transitionMessage)
-        : description(description), choices(choices), transitionMessage(transitionMessage) {}
+    Scene(const std::string& description, const std::map<char, std::tuple<std::string, std::string, int, int, int, std::string>>& choices, const std::string& transitionMessage, const std::map<char, std::string>& effectMessages)
+        : description(description), choices(choices), transitionMessage(transitionMessage), effectMessages(effectMessages) {}
 
+    int display(const Player& player) const;
     int display() const;
     std::map<char, std::tuple<std::string, std::string, int, int, int, std::string>> getChoices() const;
     char getChoice() const;
-    bool isValidChoice(char choice) const;
+     bool isValidChoice(char choice, const Player& player) const;
 
     int getHealthChange(char choice) const { return std::get<2>(choices.at(choice)); }
     int getHungerChange(char choice) const { return std::get<3>(choices.at(choice)); }
     int getMoneyChange(char choice) const { return std::get<4>(choices.at(choice)); }
     std::string getWeaponChange(char choice) const { return std::get<5>(choices.at(choice)); }
     std::string getTransitionMessage() const { return transitionMessage; }
+    std::string getEffectMessage(char choice) const {
+        if (effectMessages.find(choice) != effectMessages.end()) {
+            return effectMessages.at(choice);
+        }
+        else {
+            return "";
+        }
+    }
 
 private:
     std::string description;
-    std::string transitionMessage; // 다음 씬으로 넘어갈 때 출력할 메시지
+    std::string transitionMessage;
     std::map<char, std::tuple<std::string, std::string, int, int, int, std::string>> choices;
+    std::map<char, std::string> effectMessages;
 };
+
 
 class Game {
 public:
-    Game() : player(3, 3, 0, "None") {}  // 기본 Player 상태 설정
+    Game() : player(3, 3, 3, 3, 2, 3, "None") {}  // 기본 Player 상태 설정
 
     void addScene(const std::string& sceneId, const Scene& scene);
     void start(const std::string& startSceneId);
@@ -73,9 +99,9 @@ private:
 
 void runGame();
 
-void drawHearts(int x, int y, int health);
-void drawHunger(int x, int y, int hunger);
-void drawMoney(int x, int y, int money);
-void drawWeapon(int x, int y, const std::string& weaponName);
+void drawHearts(int x, int y, int health, int maxHealth);
+void drawHunger(int x, int y, int hunger, int maxHunger);
+void drawMoney(int x, int y, int money, int maxMoney);
+void drawWeapon(int x, int y, const std::map<std::string, int>& items);
 
 #endif
